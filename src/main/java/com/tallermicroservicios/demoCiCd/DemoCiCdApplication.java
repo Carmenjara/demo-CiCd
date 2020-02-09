67f5c7148a6b82f6d28867e03c7fd440df2ac577
bdportilla1 <bryantporti@gmail.com>
2020-02-09 18:08:48 -0500

    Subiendo primera vez

---
+++ src/main/java/com/tallermicroservicios/demoCiCd/DemoCiCdApplication.java
@@ -0,0 +1,22 @@
+package com.tallermicroservicios.demoCiCd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoCiCdApplication {
+	
+	@GetMapping("/")
+	public String home() {
+		return "Hola";
+		
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tallermicroservicios/demoCiCd/DemoCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tallermicroservicios.demoCiCd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
